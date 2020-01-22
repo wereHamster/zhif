05828d0bf0b000ce26b81325bed4b4d224701792
Tomas Carnecky <tomc@caurea.org>
2020-01-22 19:03:46 +0100

    Prepare for release

---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import { terser } from "rollup-plugin-terser";
+import copy from "rollup-plugin-copy";
+
+export default [
+  {
+    input: "src/macro/index.js",
+    output: {
+      file: "packages/macro/index.js",
+      format: "commonjs"
+    },
+    plugins: [
+      resolve(),
+      commonjs(),
+      terser(),
+      copy({
+        targets: [{ src: "src/macro/index.d.ts", dest: "packages/macro/" }]
+      })
+    ],
+    external: [
+      ...require("builtin-modules"),
+      ...Object.keys(require("./packages/macro/package.json").peerDependencies)
+    ]
+  }
+];
