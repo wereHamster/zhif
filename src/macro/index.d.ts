ad29a2cfce5a78f894743a9856a892df3503264d
Tomas Carnecky <tomc@caurea.org>
2020-02-14 10:35:18 +0100

    Export importImage return type

--- src/macro/index.d.ts
+++ src/macro/index.d.ts
@@ -1,9 +1,11 @@
 import * as React from "react";
 
-export const importImage: (src: string) => {
-  metadata: { width: number; height: number; };
+export interface Image {
+  metadata: { width: number; height: number };
   img: { src: string };
-  sources: Array<{ srcSet: string, type: string }>;
-};
+  sources: Array<{ srcSet: string; type: string }>;
+}
+
+export const importImage: (src: string) => Image;
 
 export const Picture: React.ComponentType<{ src: string } & React.ComponentPropsWithoutRef<"img">>;
