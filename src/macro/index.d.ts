f05471b35e43f01e653ef1a37733e2633091d344
Tomas Carnecky <tomc@caurea.org>
2020-04-08 19:35:45 +0200

    Document the macros

--- src/macro/index.d.ts
+++ src/macro/index.d.ts
@@ -1,11 +1,29 @@
 import * as React from "react";
 
+interface Metadata {
+  width: number;
+  height: number;
+}
+
 export interface Image {
-  metadata: { width: number; height: number };
+  metadata: Metadata;
   img: { src: string };
   sources: Array<{ srcSet: string; type: string }>;
 }
 
+/**
+ * Import an image. The returned object contains all infomration about the image and
+ * the generated (resized) versions so that you can create a proper <picture> element
+ * (or something else, such as generate CSS).
+ *
+ * @param src Relative path to the source image.
+ */
 export const importImage: (src: string) => Image;
 
-export const Picture: React.ComponentType<{ src: string } & React.ComponentPropsWithoutRef<"img">>;
+/**
+ * The <Picture> component is replaced by the <picture> element, which references
+ * the given image. The 'src' prop is required and must be a (relative) path to the
+ * image. All additional props are forwarded to the <img> element inside the
+ * <picture>.
+ */
+export const Picture: React.ComponentType<{ src: string } & JSX.IntrinsicElements["img"]>;
