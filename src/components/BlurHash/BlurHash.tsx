ad23efabc0b4239554c3e4ab24ac4daf423eca45
Tomas Carnecky <tomc@caurea.org>
2020-05-09 10:45:18 +0200

    Clean up BlurHash component

--- src/components/BlurHash/BlurHash.tsx
+++ src/components/BlurHash/BlurHash.tsx
@@ -1,10 +1,18 @@
-import { decode } from "blurhash";
 import React from "react";
 import { useInView } from "react-intersection-observer";
+import { useBlurHash } from "./useBlurHash";
 
 interface Props {
   children: React.ReactElement;
 }
+
+/*
+
+<BlurHash>
+  <Picture />
+</BlurHash>
+
+*/
 
 function BlurHash({ children: picture }: Props) {
   const { ["data-blur-hash"]: blurHash } = picture.props;
@@ -12,57 +20,49 @@ function BlurHash({ children: picture }: Props) {
   const [loaded, setLoaded] = React.useState(false);
   const [pictureRef, inView] = useInView({ triggerOnce: true });
   const blurHashURL = useBlurHash(!loaded && inView ? blurHash : null);
-  // console.log({
-  //   imageLoaded: loaded,
-  //   inView,
-  //   blurHashURL,
-  //   ratio: entry?.intersectionRatio,
-  // });
 
-  const imgRef = React.useCallback(
-    (ref: null | HTMLImageElement) => {
-      if (ref) {
-        if (ref.complete) {
-          setLoaded(true);
-        } else {
-          ref.addEventListener(
-            "load",
-            () => {
-              setLoaded(true);
-            },
-            { once: true }
-          );
-        }
+  const imgRef = React.useCallback((ref: null | HTMLImageElement) => {
+    if (ref) {
+      if (ref.complete) {
+        setLoaded(true);
       } else {
-        setLoaded(false);
+        ref.addEventListener("load", () => setLoaded(true), { once: true });
       }
-    },
-    [setLoaded]
-  );
+    } else {
+      setLoaded(false);
+    }
+  }, []);
 
   return React.cloneElement(
     picture,
-    { ref: pictureRef, style: styles.picture },
-    <div
-      style={{
-        ...styles.bg,
-        opacity: loaded ? 0 : 1,
-        backgroundImage: blurHashURL ? `url("${blurHashURL}")` : undefined,
-      }}
-    />,
+    {
+      ["data-blur-hash"]: undefined,
+      ref: pictureRef,
+      style: styles.picture,
+    },
+    blurHashURL && (
+      <div
+        style={{
+          ...styles.bg,
+          opacity: loaded ? 0 : 1,
+          backgroundImage: `url("${blurHashURL}")`,
+        }}
+      />
+    ),
     ...React.Children.toArray(picture.props.children).map((child) => {
-      if (React.isValidElement(child) && child.type === "img") {
-        return React.cloneElement(child, {
-          ref: imgRef,
-          loading: "lazy",
-          style: {
-            ...styles.img,
-            opacity: loaded ? 1 : 0,
-            filter: inView ? "none" : "blur(3px)",
-          },
-        } as any);
-      } else if (!inView && React.isValidElement(child)) {
-        return React.cloneElement(child as any, { sizes: "50px" });
+      if (React.isValidElement(child)) {
+        if (child.type === "img" || child.props.originalType === "img") {
+          return React.cloneElement(child, {
+            ref: imgRef,
+            style: {
+              ...styles.img,
+              opacity: loaded ? 1 : 0,
+              ...child.props.style,
+            },
+          });
+        } else {
+          return child;
+        }
       } else {
         return child;
       }
@@ -76,7 +76,6 @@ const styles = {
   picture: {
     position: "relative",
     display: "block",
-    overflow: "hidden",
   },
   bg: {
     pointerEvents: "none",
@@ -95,54 +94,3 @@ const styles = {
     transition: "opacity .5s",
   },
 } as const;
-
-// modified from https://gist.github.com/WorldMaker/a3cbe0059acd827edee568198376b95a
-// https://github.com/woltapp/react-blurhash/issues/3
-export function useBlurHash(
-  blurhash: string | undefined | null,
-  width: number = 32,
-  height: number = 32,
-  punch: number = 1
-) {
-  const [url, setUrl] = React.useState(null as string | null);
-
-  React.useEffect(() => {
-    let isCancelled = false;
-
-    if (!blurhash) return;
-
-    // decode hash
-    const pixels = decode(blurhash, width, height, punch);
-
-    // temporary canvas to create a blob from decoded ImageData
-    const canvas = document.createElement("canvas");
-    canvas.width = width;
-    canvas.height = height;
-    const context = canvas.getContext("2d");
-    const imageData = context!.createImageData(width, height);
-    imageData.data.set(pixels);
-    context!.putImageData(imageData, 0, 0);
-    canvas.toBlob((blob) => {
-      if (!isCancelled) {
-        setUrl((oldUrl) => {
-          if (oldUrl) {
-            URL.revokeObjectURL(oldUrl);
-          }
-          return URL.createObjectURL(blob);
-        });
-      }
-    });
-
-    return function cleanupBlurhash() {
-      isCancelled = true;
-      setUrl((oldUrl) => {
-        if (oldUrl) {
-          URL.revokeObjectURL(oldUrl);
-        }
-        return null;
-      });
-    };
-  }, [blurhash, height, width, punch]);
-
-  return url;
-}
---
+++ src/components/BlurHash/useBlurHash.ts
@@ -0,0 +1,53 @@
+import { decode } from "blurhash";
+import React from "react";
+
+// modified from https://gist.github.com/WorldMaker/a3cbe0059acd827edee568198376b95a
+// https://github.com/woltapp/react-blurhash/issues/3
+export function useBlurHash(
+  blurhash: string | undefined | null,
+  width: number = 32,
+  height: number = 32,
+  punch: number = 1
+) {
+  const [url, setUrl] = React.useState<null | string>(null);
+
+  React.useEffect(() => {
+    if (!blurhash) return;
+
+    let isCancelled = false;
+
+    // decode hash
+    const pixels = decode(blurhash, width, height, punch);
+
+    // temporary canvas to create a blob from decoded ImageData
+    const canvas = document.createElement("canvas");
+    canvas.width = width;
+    canvas.height = height;
+    const context = canvas.getContext("2d");
+    const imageData = context!.createImageData(width, height);
+    imageData.data.set(pixels);
+    context!.putImageData(imageData, 0, 0);
+    canvas.toBlob((blob) => {
+      if (!isCancelled) {
+        setUrl((oldUrl) => {
+          if (oldUrl) {
+            URL.revokeObjectURL(oldUrl);
+          }
+          return URL.createObjectURL(blob);
+        });
+      }
+    });
+
+    return function cleanupBlurhash() {
+      isCancelled = true;
+      setUrl((oldUrl) => {
+        if (oldUrl) {
+          URL.revokeObjectURL(oldUrl);
+        }
+        return null;
+      });
+    };
+  }, [blurhash, height, width, punch]);
+
+  return url;
+}
