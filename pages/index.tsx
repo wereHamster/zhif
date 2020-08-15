77bc01defce26a386debd6bab92a3c8194175e6f
Tomas Carnecky <tomc@caurea.org>
2020-08-15 19:59:18 +0200

    Use a different (stable) source image in tests

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import { importImage, Picture } from "../src/macro";
 
 const image1 = importImage("../assets/image.jpg");
 console.log(image1);
-const image2 = importImage("https://i.picsum.photos/id/1/1200/640.jpg");
+const image2 = importImage("https://caurea.org/zhif.jpg");
 console.log(image2)
 
 // const picture3 = picture("../image.jpg", {
@@ -28,83 +28,83 @@ export default () => (
       }}
     >
       <BlurHash>
-        <Picture src="https://i.picsum.photos/id/1/1200/640.jpg" sizes="360px" />
+        <Picture src="https://caurea.org/zhif.jpg" sizes="360px" />
       </BlurHash>
       <BlurHash>
-        <Picture src="https://i.picsum.photos/id/2/1200/640.jpg" sizes="360px" />
+        <Picture src="https://caurea.org/zhif.jpg" sizes="360px" />
       </BlurHash>
       <BlurHash>
-        <Picture src="https://i.picsum.photos/id/3/1200/640.jpg" sizes="360px" />
+        <Picture src="https://caurea.org/zhif.jpg" sizes="360px" />
       </BlurHash>
       <BlurHash>
-        <Picture src="https://i.picsum.photos/id/4/1200/640.jpg" sizes="360px" />
+        <Picture src="https://caurea.org/zhif.jpg" sizes="360px" />
       </BlurHash>
       <BlurHash>
-        <Picture src="https://i.picsum.photos/id/5/1200/640.jpg" sizes="360px" />
+        <Picture src="https://caurea.org/zhif.jpg" sizes="360px" />
       </BlurHash>
       <BlurHash>
-        <Picture src="https://i.picsum.photos/id/6/1200/640.jpg" sizes="360px" />
+        <Picture src="https://caurea.org/zhif.jpg" sizes="360px" />
       </BlurHash>
       <BlurHash>
-        <Picture src="https://i.picsum.photos/id/7/1200/640.jpg" sizes="360px" />
+        <Picture src="https://caurea.org/zhif.jpg" sizes="360px" />
       </BlurHash>
       <BlurHash>
-        <Picture src="https://i.picsum.photos/id/8/1200/640.jpg" sizes="360px" />
+        <Picture src="https://caurea.org/zhif.jpg" sizes="360px" />
       </BlurHash>
       <BlurHash>
-        <Picture src="https://i.picsum.photos/id/9/1200/640.jpg" sizes="360px" />
+        <Picture src="https://caurea.org/zhif.jpg" sizes="360px" />
       </BlurHash>
       <BlurHash>
-        <Picture src="https://i.picsum.photos/id/10/1200/640.jpg" sizes="360px" />
+        <Picture src="https://caurea.org/zhif.jpg" sizes="360px" />
       </BlurHash>
       <BlurHash>
-        <Picture src="https://i.picsum.photos/id/11/1200/640.jpg" sizes="360px" />
+        <Picture src="https://caurea.org/zhif.jpg" sizes="360px" />
       </BlurHash>
       <BlurHash>
-        <Picture src="https://i.picsum.photos/id/12/1200/640.jpg" sizes="360px" />
+        <Picture src="https://caurea.org/zhif.jpg" sizes="360px" />
       </BlurHash>
 
       {Array.from({ length: 40 }).fill(img)}
 
       <BlurHash>
-        <Picture src="https://i.picsum.photos/id/1001/1200/640.jpg" sizes="360px" />
+        <Picture src="https://caurea.org/zhif.jpg" sizes="360px" />
       </BlurHash>
       <BlurHash>
-        <Picture src="https://i.picsum.photos/id/1002/1200/640.jpg" sizes="360px" />
+        <Picture src="https://caurea.org/zhif.jpg" sizes="360px" />
       </BlurHash>
       <BlurHash>
-        <Picture src="https://i.picsum.photos/id/1003/1200/640.jpg" sizes="360px" />
+        <Picture src="https://caurea.org/zhif.jpg" sizes="360px" />
       </BlurHash>
       <BlurHash>
-        <Picture src="https://i.picsum.photos/id/1004/1200/640.jpg" sizes="360px" />
+        <Picture src="https://caurea.org/zhif.jpg" sizes="360px" />
       </BlurHash>
       <BlurHash>
-        <Picture src="https://i.picsum.photos/id/1005/1200/640.jpg" sizes="360px" />
+        <Picture src="https://caurea.org/zhif.jpg" sizes="360px" />
       </BlurHash>
       <BlurHash>
-        <Picture src="https://i.picsum.photos/id/1006/1200/640.jpg" sizes="360px" />
+        <Picture src="https://caurea.org/zhif.jpg" sizes="360px" />
       </BlurHash>
        <BlurHash>
-        <Picture src="https://i.picsum.photos/id/1008/1200/640.jpg" sizes="360px" />
+        <Picture src="https://caurea.org/zhif.jpg" sizes="360px" />
       </BlurHash>
      <BlurHash>
-        <Picture src="https://i.picsum.photos/id/1009/1200/640.jpg" sizes="360px" />
+        <Picture src="https://caurea.org/zhif.jpg" sizes="360px" />
       </BlurHash>
       <BlurHash>
-        <Picture src="https://i.picsum.photos/id/1010/1200/640.jpg" sizes="360px" />
+        <Picture src="https://caurea.org/zhif.jpg" sizes="360px" />
       </BlurHash>
       <BlurHash>
-        <Picture src="https://i.picsum.photos/id/1011/1200/640.jpg" sizes="360px" />
+        <Picture src="https://caurea.org/zhif.jpg" sizes="360px" />
       </BlurHash>
       <BlurHash>
-        <Picture src="https://i.picsum.photos/id/1012/1200/640.jpg" sizes="360px" />
+        <Picture src="https://caurea.org/zhif.jpg" sizes="360px" />
       </BlurHash>
     </div>
     <div>
       <BlurHash>
         <Picture
           id="1"
-          src="https://exposure.imgix.net/production/posts/241311/cover-photo/cover-1499518139.png"
+          src="https://caurea.org/zhif.jpg"
           alt="Flower"
         />
       </BlurHash>
